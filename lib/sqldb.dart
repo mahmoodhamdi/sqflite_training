82280becb53b1143e66009eb0bf80e16137fef8f
mahmoodhamdi <mahmouashwah@gmail.com>
2023-10-30 00:19:53 +0200

    FORMAT CODE

--- lib/sqldb.dart
+++ lib/sqldb.dart
@@ -4,27 +4,28 @@ import 'package:sqflite_training/constants.dart';
 
 class SqlDb {
   static Database? _db;
-Future<Database?>get db async{
-  if(_db==null){
-    _db=await initialDb();
-    return _db;
-  }else {
-    return _db;
+  Future<Database?> get db async {
+    if (_db == null) {
+      _db = await initialDb();
+      return _db;
+    } else {
+      return _db;
+    }
   }
-}
 
- 
   initialDb() async {
     String dataBasePath = await getDatabasesPath();
     String path = join(dataBasePath, dataBaseName);
-    Database database = await openDatabase(path, onCreate: onCreate,version: 1,onUpgrade:onUpgrade );
+    Database database = await openDatabase(path,
+        onCreate: onCreate, version: 1, onUpgrade: onUpgrade);
     return database;
   }
-  onUpgrade(Database database,int oldVersion,int newVersion) {
 
-print("onUpgrade==========================>");
+  onUpgrade(Database database, int oldVersion, int newVersion) {
+    print("onUpgrade==========================>");
   }
-onCreate(Database database, int version) async {
+
+  onCreate(Database database, int version) async {
     await database.execute('''
 Create table  "notes" (
   "id" INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -34,26 +35,27 @@ Create table  "notes" (
     print("on create database=====================>");
   }
 
-  readData(String sql)async{
-    Database?myDb=await db;
-    List<Map> response=await myDb!.rawQuery(sql);
+  readData(String sql) async {
+    Database? myDb = await db;
+    List<Map> response = await myDb!.rawQuery(sql);
     return response;
-  }  insertData(String sql)async{
-    Database?myDb=await db;
-    int response=await myDb!.rawInsert(sql);
-    return response;
-
-  }  deleteData(String sql)async{
-    Database?myDb=await db;
-   int response=await   myDb!.rawDelete(sql);
-    return response;
-    
-
   }
-    updateData(String sql)async{
-    Database?myDb=await db;
-    int response=await myDb!.rawUpdate(sql);
-    return response;
 
+  insertData(String sql) async {
+    Database? myDb = await db;
+    int response = await myDb!.rawInsert(sql);
+    return response;
+  }
+
+  deleteData(String sql) async {
+    Database? myDb = await db;
+    int response = await myDb!.rawDelete(sql);
+    return response;
+  }
+
+  updateData(String sql) async {
+    Database? myDb = await db;
+    int response = await myDb!.rawUpdate(sql);
+    return response;
   }
 }
